247c10896e4cca2569e2ede342139fe7a8710d57
spitzer9508 <spitzer9508@gmail.com>
2026-09-15 21:02:32 +0100

    feat: configure V1 prediction competitions

---
+++ config/competitions.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'v1' => [
+        ['country' => 'England', 'names' => ['Premier League']],
+        ['country' => 'Spain', 'names' => ['LaLiga', 'La Liga']],
+        ['country' => 'Germany', 'names' => ['Bundesliga']],
+        ['country' => 'Italy', 'names' => ['Serie A']],
+        ['country' => 'France', 'names' => ['Ligue 1']],
+        ['country' => 'Europe', 'names' => ['Champions League', 'UEFA Champions League']],
+    ],
+    'statistics_per_run' => 20,
+];
